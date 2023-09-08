a586b1bd59f66247be8b4cf17ae2681eb062024c
punyaNaya <144455372+punyaNaya@users.noreply.github.com>
2023-09-08 20:19:36 +0700

    Create kamusgaul.py

---
+++ kamusgaul.py
@@ -0,0 +1,16 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "NGELEM":"perilaku yang aneh",
+            }
+            
+            
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+
+
+if word in meme_dict.keys():
+    # Apa yang harus kita lakukan jika kata itu ditemukan?
+    print(meme_dict[word])
+else:
+    # Apa yang harus kita lakukan jika kata itu tidak ditemuka
+    print('kata tidak di temukan')
